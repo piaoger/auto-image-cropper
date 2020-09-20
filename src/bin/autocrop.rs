090b3950981636c6e4a0d88f2c1f4c7f05c81efd
piaoger gong <piaoger@piaogers-MacBook-Pro.local>
2020-09-20 21:06:09 +0800

    refact:update deps and run fmt

--- src/bin/autocrop.rs
+++ src/bin/autocrop.rs
@@ -1,6 +1,6 @@
+extern crate autocrop;
 extern crate clap;
 extern crate rayon;
-extern crate autocrop;
 
 use clap::{App, Arg};
 use rayon::prelude::*;
@@ -8,58 +8,58 @@ use std::fs;
 use std::fs::DirEntry;
 use std::path::Path;
 
-
 fn main() {
-	/* Hold Your Breath */
-	let arguments = App::new("auto-image-cropper")
-		.version("0.1.5")
-		.author("Ritiek Malhotra <ritiekmalhotra123@gmail.com>")
-		.about("Removes extra white boundaries from images to correctly resize canvas.")
-		.arg(
-			Arg::with_name("input")
-				.short("i")
-				.long("input")
-				.value_name("LOCATION")
-				.required(true)
-				.takes_value(true)
-				.help("Location of input image/directory"),
-		)
-		.arg(
-			Arg::with_name("output")
-				.short("o")
-				.long("output")
-				.value_name("LOCATION")
-				.takes_value(true)
-				.help("Location of output image/directory"),
-		)
-		.get_matches();
+    /* Hold Your Breath */
+    let arguments = App::new("auto-image-cropper")
+        .version("0.1.5")
+        .author("Ritiek Malhotra <ritiekmalhotra123@gmail.com>")
+        .about("Removes extra white boundaries from images to correctly resize canvas.")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("LOCATION")
+                .required(true)
+                .takes_value(true)
+                .help("Location of input image/directory"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("LOCATION")
+                .takes_value(true)
+                .help("Location of output image/directory"),
+        )
+        .get_matches();
 
-	let input_path = arguments.value_of("input").unwrap();
-	let output_path = arguments.value_of("output").unwrap_or(input_path);
-	let path_type = fs::metadata(input_path).unwrap();
+    let input_path = arguments.value_of("input").unwrap();
+    let output_path = arguments.value_of("output").unwrap_or(input_path);
+    let path_type = fs::metadata(input_path).unwrap();
 
-	if path_type.is_file() {
-		autocrop::crop_image(input_path, output_path);
+    if path_type.is_file() {
+        autocrop::crop_image(input_path, output_path);
+    } else {
+        let input_files = fs::read_dir(input_path).unwrap();
 
-	} else {
-		let input_files = fs::read_dir(input_path).unwrap();
+        if !Path::new(output_path).exists() {
+            let _ = fs::create_dir(output_path);
+        }
+        let mut files_to_crop: Vec<DirEntry> = Vec::with_capacity(10);
+        for file in input_files {
+            files_to_crop.push(file.unwrap());
+        }
+        println!("Vector length is {}", files_to_crop.len());
 
-		if !Path::new(output_path).exists() {
-			let _ = fs::create_dir(output_path);
-		}
-		let mut files_to_crop: Vec<DirEntry> = Vec::with_capacity(10);
-		for file in input_files {
-			files_to_crop.push(file.unwrap());
-		}
-		println!("Vector length is {}", files_to_crop.len());
+        files_to_crop.par_iter().for_each(|file| {
+            let img_in = file.path();
+            let img_name = img_in.file_name();
+            let img_out = Path::new(output_path).join(img_name.unwrap());
 
-		files_to_crop.par_iter().for_each(|file| {
-			let img_in = file.path();
-			let img_name = img_in.file_name();
-			let img_out = Path::new(output_path).join(img_name.unwrap());
-
-			autocrop::crop_image(&(img_in.display().to_string()), &(img_out.into_os_string().into_string().unwrap()));
-		});
-	}
-
+            autocrop::crop_image(
+                &(img_in.display().to_string()),
+                &(img_out.into_os_string().into_string().unwrap()),
+            );
+        });
+    }
 }
